33de0763beab6fac8b86568ce43bd606c642f840
Julian Salazar <julian@salazar.io>
2014-04-19 12:14:26 -0700

    make support server work, propagate open event

--- test/support/server.js
+++ test/support/server.js
@@ -9,7 +9,8 @@ var http = require('http').Server(app);
 var server = require('engine.io').attach(http);
 var browserify = require('../../support/browserify');
 
-http.listen(process.env.ZUUL_PORT);
+// http.listen(process.env.ZUUL_PORT);
+http.listen(9005);
 
 // server worker.js as raw file
 app.use('/test/support', express.static(join(__dirname, 'public')));
